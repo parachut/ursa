fdc5cafe17bd52c392f9350cbed902b5e3d6eace
Daniel Wallace <41707979+parachut-daniel@users.noreply.github.com>
2020-02-06 13:53:30 -0500

    .

--- apps/forage/src/recurly/recurly.module.ts
+++ apps/forage/src/recurly/recurly.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RecurlyController } from './recurly.controller';
 import { RecurlyService } from './recurly.service';
-
+import { DatabaseModule } from '@app/database';
 @Module({
+  imports: [DatabaseModule],
   providers: [RecurlyService],
   controllers: [RecurlyController],
 })
--- apps/forage/src/recurly/recurly.service.ts
+++ apps/forage/src/recurly/recurly.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Subscription } from '@app/database/entities';
 
 @Injectable()
 export class RecurlyService {
+  private readonly subscriptionRepository: typeof Subscription = this.sequelize.getRepository(
+    'Subscription',
+  );
+
+  constructor(@Inject('SEQUELIZE') private readonly sequelize) {}
+
+  async findSubscriptions(): Promise<Subscription[]> {
+    const subscriptions = await this.subscriptionRepository.findAll({});
+    return subscriptions;
+  }
+
   getHello(): string {
     return 'Hello Recurly!';
   }
