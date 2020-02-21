1d7770398af4407e227619571db0e0ae168e9d08
Bartosz Zarzecki <bart@parachut.co>
2020-02-21 11:32:51 -0500

    .

--- apps/forest/src/forest/product.ts
+++ apps/forest/src/forest/product.ts
@@ -1,24 +1,32 @@
 import * as Liana from 'forest-express-sequelize';
 
 Liana.collection('Product', {
-    actions: [
-        {
-            name: 'Insert Product',
-            type: 'global',
+  actions: [
+    {
+      name: 'Insert Product',
+      type: 'global',
 
-            fields: [
-                {
-                    field: 'url',
-                    type: 'String',
-                    isRequired: true,
-                    description: "Insert product url from B&H website."
-                },
-                {
-                    field: 'price',
-                    type: 'Number',
-                    description: "Insert price, if the price does not appear on the B&H page"
-                },
-            ],
+      fields: [
+        {
+          field: 'url',
+          type: 'String',
+          isRequired: true,
+          description: "Insert product url from B&H website."
         },
-    ],
+        {
+          field: 'price',
+          type: 'Number',
+          description: "Insert price, if the price does not appear on the B&H page"
+        },
+      ],
+    },
+    {
+      name: 'Insert Values',
+      type: 'global',
+    },
+    {
+      name: 'mpb',
+      type: 'global',
+    }
+  ],
 });
